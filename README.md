390a3aa2e61cb65746ace838a6c7dfb9ec491e9d
Kinsey Van Ost <kinsey.vanost@gmail.com>
2016-05-25 03:53:34 -0700

    Adds initial config to project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 A playground featuring the power of [react](https://facebook.github.io/react/), [redux](http://redux.js.org/) and [immutablejs](https://facebook.github.io/immutable-js/).
+
+Linting in atom can be enabled by using:
+`apm install linter-eslint`
--- src/store.js
+++ src/store.js
@@ -1,11 +1,16 @@
-import thunk from 'redux-thunk';
-import immutable from 'immutable';
-import { createStore, applyMiddleware } from 'redux';
+import Immutable from 'immutable';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { combineReducers } from 'redux-immutablejs';
 import * as reducers from './reducers';
+import initialStateObject from './initialState';
+import middleware from './middleware';
 
-const reducer = combineReducers(reducers);
-const state = immutable.Map({});
-const store = reducer(state);
+const rootReducer = combineReducers(reducers);
+const initialState = Immutable.fromJS(initialStateObject);
 
-export default applyMiddleware(thunk)(createStore)(reducer, state);
+const store = createStore(rootReducer, initialState, compose(
+  applyMiddleware(...middleware),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+export default store;
