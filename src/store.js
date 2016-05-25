68567583954361b96072327879b78cfafdcbaa3e
Kinsey Van Ost <kinsey.vanost@gmail.com>
2016-05-25 04:01:42 -0700

    Temporarily removes middleware dir in exchange for thunk

--- src/store.js
+++ src/store.js
@@ -3,13 +3,14 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { combineReducers } from 'redux-immutablejs';
 import * as reducers from './reducers';
 import initialStateObject from './initialState';
-import middleware from './middleware';
+// import middleware from './middleware';
+import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers(reducers);
 const initialState = Immutable.fromJS(initialStateObject);
 
 const store = createStore(rootReducer, initialState, compose(
-  applyMiddleware(...middleware),
+  applyMiddleware(thunk),
   window.devToolsExtension ? window.devToolsExtension() : f => f
 ));
 
