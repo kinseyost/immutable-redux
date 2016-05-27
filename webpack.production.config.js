e96942ec217c023df7cfbcf65d985ca437f43b4b
Kinsey Van Ost <kinsey.vanost@gmail.com>
2016-05-27 08:11:54 -0700

    Adds config for production

---
+++ webpack.production.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  devtool: 'cheap-source-map',
+  entry: [
+    './src/index',
+  ],
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: [path.resolve(__dirname, 'node_modules')], // hopefully this works.
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract(
+          'style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'
+        ),
+      },
+    ],
+  },
+  postcss: [require('autoprefixer')],
+  plugins: [
+    new ExtractTextPlugin('style.css', { allChunks: true }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false,
+      },
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+  ],
+};
