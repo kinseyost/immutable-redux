0943d5e21a671d620cb1216841becde7f58b0a5f
Kinsey Van Ost <kinsey.vanost@gmail.com>
2016-05-26 20:59:15 -0700

    Page Change now works

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
+import React, { PropTypes } from 'react';
 import styles from './App.css';
+import { Link } from 'react-router';
 
+const propTypes = {
+  children: PropTypes.array,
+};
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className={ styles.Body }>
-       hello world
-      </div>
-    );
-  }
+export default function App(props) {
+  return (
+    <div className={ styles.Body }>
+      <ul>
+        <li><Link to="/page1">About</Link></li>
+        <li><Link to="/page2">Inbox</Link></li>
+      </ul>
+      { props.children }
+    </div>
+  );
 }
+
+App.propTypes = propTypes;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 import store from './store';
 import App from './App';
+import Page1 from './components/Page1.js';
+import Page2 from './components/Page2.js';
 
 ReactDOM.render(
   <Provider store={ store }>
-    <Router history={ hashHistory }>
-      <Route path="/" component={ App } />
+    <Router history={ browserHistory }>
+      <Route path="/" component={ App }>
+        <Route path="page1" component={ Page1 } />
+        <Route path="page2" component={ Page2 } />
+      </Route>
     </Router>
   </Provider>
   , document.getElementById('root')
