c1fe9b0354e45bd95e34f056cca823e44c6c80b0
kineyost <kinsey.vanost@gmail.com>
2016-06-01 07:10:55 -0700

    Changes double to single quotes for jsx.

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ import Page2 from './components/Page2.js';
 ReactDOM.render(
   <Provider store={ store }>
     <Router history={ browserHistory }>
-      <Route path="/" component={ App }>
-        <Route path="page1" component={ Page1 } />
-        <Route path="page2" component={ Page2 } />
+      <Route path='/' component={ App }>
+        <Route path='page1' component={ Page1 } />
+        <Route path='page2' component={ Page2 } />
       </Route>
     </Router>
   </Provider>
