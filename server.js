bc2e3d6ebfa45d13c33b92839cec4c03c97ed074
kineyost <kinsey.vanost@gmail.com>
2016-06-04 12:42:00 -0700

    Adds production env. listener to server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
+const port = process.env.NODE_ENV === 'production' ? process.env.PORT : 3000;
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+}).listen(port, 'localhost', function (err, result) {
   if (err) {
     return console.log(err);
   }
 
-  console.log('Listening at http://localhost:3000/');
+  console.log('Listening at http://localhost:/' + port);
 });
