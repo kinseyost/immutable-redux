00c11c7f467839022c382d56225e44224cef9059
kineyost <kinsey.vanost@gmail.com>
2016-06-08 22:58:24 -0700

    Adds express for production

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
+var express = require('express');
+var app = express();
+
 const port = process.env.NODE_ENV === 'production' ? process.env.PORT : 3000;
 if (process.env.NODE_ENV !== 'production') {
- new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(port, 'localhost', function (err, result) {
-  if (err) {
-    return console.log(err);
-  }
+  new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true
+  }).listen(port, 'localhost', function (err, result) {
+    if (err) {
+      return console.log(err);
+    }
 
-  console.log('Listening at http://localhost:/' + port);
- }
-});
+    console.log('Listening at http://localhost:/' + port);
+  });
+} else {
+  app.use(express.static(__dirname + '/public'));
+
+  app.get('/', function(request, response) {
+    response.render('src/index');
+  });
+
+  app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+  });
+}
