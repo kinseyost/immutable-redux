0f7a77294b788a5a31a4590831b7b8f3f5cf95c8
kineyost <kinsey.vanost@gmail.com>
2016-06-09 18:21:33 -0700

    moves dev dependencies into dev block

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
-var express = require('express');
-var app = express();
-
 const port = process.env.NODE_ENV === 'production' ? process.env.PORT : 3000;
+
 if (process.env.NODE_ENV !== 'production') {
+  var config = require('./webpack.config');
+  var webpack = require('webpack');
+  var WebpackDevServer = require('webpack-dev-server');
   new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     hot: true,
@@ -18,13 +16,10 @@ if (process.env.NODE_ENV !== 'production') {
     console.log('Listening at http://localhost:/' + port);
   });
 } else {
+  var express = require('express');
+  var app = express();
   app.use(express.static(__dirname + '/public'));
-
-  app.get('/', function(request, response) {
-    response.render('src/index');
-  });
-
-  app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
+  app.get('*', function response(req, res) {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
   });
 }
