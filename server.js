ed6550350e92e8fa524ef870378485b94113022a
kineyost <kinsey.vanost@gmail.com>
2016-06-17 22:44:53 -0700

    Changes production to public

--- server.js
+++ server.js
@@ -30,9 +30,9 @@ if (isDeveloping) {
     res.end();
   });
 } else {
-  app.use(express.static(__dirname + '/dist'));
+  app.use(express.static(__dirname + '/public'));
   app.get('*', function response(req, res) {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
   });
 }
 
