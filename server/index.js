f2365530171a61a0ba0d4d7d9bcb0114fa4bd49f
kineyost <kinsey.vanost@gmail.com>
2016-06-26 04:45:04 -0700

    Adds connection to mongodb

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var host = process.env.IP;
+var mongoose = require('mongoose');
+
 
 io.on('connection', (socket) => {
   console.log('client connected');
@@ -10,4 +12,12 @@ io.on('connection', (socket) => {
 
 http.listen(8081, function(){
   console.log(`listening on :8081`);
+});
+
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected to mongodb');
 });
