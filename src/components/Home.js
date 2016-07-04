8ffee02beddae969445e00fae92d3d765d2211e8
Kinsey Van Ost <kinsey.vanost@gmail.com>
2016-07-04 06:45:23 -0700

    Happy 4th of July

---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div> Happy 4th of July </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import store from './store';
 import App from './App';
+import Home from './components/Home.js';
 import Page1 from './components/Page1.js';
 import Page2 from './components/Page2.js';
 // styles reset
@@ -13,6 +14,7 @@ ReactDOM.render(
   <Provider store={ store }>
     <Router history={ browserHistory }>
       <Route path='/' component={ App }>
+	<IndexRoute component={ Home }/>
         <Route path='page1' component={ Page1 } />
         <Route path='page2' component={ Page2 } />
       </Route>
