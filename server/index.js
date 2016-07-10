34d900c2598f08ac8f23b715143a852092aa5eab
Kinsey Van Ost <kinsey.vanost@gmail.com>
2016-07-10 16:05:57 -0600

    Adds babel support to Server

--- server/index.js
+++ server/index.js
@@ -4,11 +4,13 @@ var io = require('socket.io')(http);
 var host = process.env.IP;
 var mongoose = require('mongoose');
 
+import { userModel } from './models';
+
+// TODO store and retreive user data from mongo
 
 io.on('connection', (socket) => {
   console.log('client connected');
 });
-
 
 http.listen(8081, function(){
   console.log(`listening on :8081`);
