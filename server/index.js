07df30390b1565cdb37a542d6cdbd6fd1f64b2a6
Kinsey Van Ost <kinsey.vanost@gmail.com>
2016-07-10 17:34:37 -0600

    Converts to ES6 syntax

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var host = process.env.IP;
-var mongoose = require('mongoose');
-
+import express from 'express';
+import http from 'http';
+import socketIO from 'socket.io';
+import mongoose from 'mongoose';
 import { userModel } from './models';
+
+const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
+const host = process.env.IP;
 
 // TODO store and retreive user data from mongo
 
@@ -12,13 +15,13 @@ io.on('connection', (socket) => {
   console.log('client connected');
 });
 
-http.listen(8081, function(){
+server.listen(8081, function(){
   console.log(`listening on :8081`);
 });
 
 mongoose.connect('mongodb://localhost/test');
 
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('connected to mongodb');
