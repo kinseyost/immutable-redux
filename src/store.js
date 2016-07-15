7a8923793e6f376540ef5c4925009579f402ba15
Kinsey Van Ost <kinsey.vanost@gmail.com>
2016-07-15 08:34:02 -0700

    Adds ioMiddleware

--- src/store.js
+++ src/store.js
@@ -2,12 +2,12 @@ import Immutable from 'immutable';
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import initialStateObject from './initialState';
-// import middleware from './middleware';
+import { ioMiddleware } from './middleware';
 import thunk from 'redux-thunk';
 
 const initialState = Immutable.fromJS(initialStateObject);
 const store = createStore(rootReducer, initialState, compose(
-  applyMiddleware(thunk),
+  applyMiddleware(thunk, ioMiddleware),
   window.devToolsExtension ? window.devToolsExtension() : f => f
 ));
 
