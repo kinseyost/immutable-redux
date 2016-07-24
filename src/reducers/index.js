f849cd7cfb2702f6859c9750a5e4e652566e551e
Kinsey Van Ost <kinsey.vanost@gmail.com>
2016-07-24 11:12:22 -0700

    Adds redux-from reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux-immutablejs';
 import userInfo from './users.js';
+import { reducer as formReducer } from 'redux-form';
+
 
 export default combineReducers({
   userInfo,
+  formReducer,
 });
