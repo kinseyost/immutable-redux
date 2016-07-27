adfc3d0c3f4aec0d58f75e50cab05c2df10ea0bf
Kinsey Van Ost <kinsey.vanost@gmail.com>
2016-07-27 07:58:32 -0700

    Removes redux form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,6 @@
 import { combineReducers } from 'redux-immutablejs';
 import userInfo from './users.js';
-import { reducer as formReducer } from 'redux-form';
-
 
 export default combineReducers({
   userInfo,
-  form: (state = Immutable.fromJS({}), action) => 
-			Immutable.fromJS(formReducer(state.toJS(), action)),
 });
