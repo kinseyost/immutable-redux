e9ff15a1be996c63a2cb06facdc918ee8f4a07ce
Kinsey Van Ost <kinsey.vanost@gmail.com>
2016-08-21 11:16:58 -0700

    Adds more theme vars

--- src/styles/vars/index.js
+++ src/styles/vars/index.js
@@ -1,4 +1,11 @@
+const borderWidth = '1px';
+
 export default {
   primaryColor: 'green',
-  border: '2px solid #bdc3c7',
+  primaryTextColor: '#666666',
+  borderRadius: '6px',
+  border: `${borderWidth} solid #bdc3c7`,
+  transparentBorder: `${borderWidth} solid transparent`,
+  fontSize: '14px',
+  headerSize: '24px',
 };
