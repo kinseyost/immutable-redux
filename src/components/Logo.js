9a048c48b03e949cad6595b632ab3b9e67c77c6e
kinseyost <kinsey.vanost@gmail.com>
2017-04-30 15:09:22 -0700

    Fixes variables and colors

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,5 +3,11 @@ import styles from './Logo.css';
 import logo from 'images/kinseyost.jpg';
 
 export default function Logo({ ...props }) {
-  return (<img src={ logo } className={ styles.Logo } { ...props } />);
+  return (
+    <img
+      src={ logo }
+      className={ styles.Logo }
+      { ...props }
+    />
+  );
 }
--- src/styles/vars/index.js
+++ src/styles/vars/index.js
@@ -1,11 +1,14 @@
 const borderWidth = '1px';
 
 export default {
-  primaryColor: 'green',
+  primaryColor: '#3d93c5',
+  primaryButtonColor: 'green',
   primaryTextColor: '#666666',
   borderRadius: '6px',
   border: `${borderWidth} solid #bdc3c7`,
   transparentBorder: `${borderWidth} solid transparent`,
   fontSize: '14px',
   headerSize: '24px',
+  imageBorder: '3px solid white',
+  primaryFont: '\'Lato\', sans-serif',
 };
